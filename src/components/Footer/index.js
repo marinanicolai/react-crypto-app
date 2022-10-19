18f046d7ea377abdea66a1bbed94b5f52ec3aeae
marina <nicolaimarina@gmail.com>
2022-10-19 14:10:47 -0400

    adding the contact info in the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -6,6 +8,23 @@ const Footer = () => {
     <>
       <main>
         <h1>Footer component</h1>
+        <a
+          href="https://www.linkedin.com/in/marina-nicolai-b627b1128/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a href="mailto:nicolaimarina@gmail.com">
+          <HiMail />
+        </a>
+        <a
+          href="https://github.com/marinanicolai"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
       </main>
     </>
   );

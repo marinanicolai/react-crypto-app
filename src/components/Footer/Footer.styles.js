7497fa38d6b0d715a76b5eb08e35b89d3703ff68
marina <nicolaimarina@gmail.com>
2022-10-24 09:29:40 -0400

    footer

---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Row } from "antd";
+
+export const Container = styled.div``;
+
+export const SocialMediaRow = styled(Row)``;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,30 +2,32 @@ import React from "react";
 import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { Container, SocialMediaRow } from "./Footer.styles";
 
 const Footer = () => {
   return (
     <>
-      <main>
-        <h1>Footer component</h1>
-        <a
-          href="https://www.linkedin.com/in/marina-nicolai-b627b1128/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-        <a href="mailto:nicolaimarina@gmail.com">
-          <HiMail />
-        </a>
-        <a
-          href="https://github.com/marinanicolai"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithubSquare />
-        </a>
-      </main>
+      <Container>
+        <SocialMediaRow>
+          <a
+            href="https://www.linkedin.com/in/marina-nicolai-b627b1128/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a href="mailto:nicolaimarina@gmail.com">
+            <HiMail />
+          </a>
+          <a
+            href="https://github.com/marinanicolai"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithubSquare />
+          </a>
+        </SocialMediaRow>
+      </Container>
     </>
   );
 };
